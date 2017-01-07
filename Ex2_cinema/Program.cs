d9e1de05483e88e024c8e9654bb7f3cfb0d1dcdc
ChesMC <wolf_zem_@mail.ru>
2017-01-08 01:19:59 +0300

    create new file

---
+++ Ex2_cinema/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2_cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа подбора кино");
+            Console.WriteLine("Введите ваше имя:");
+            String st = Console.ReadLine();
+            String str = "Hi, " + st;
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
